2603d323f2cbc5585e8427a55a555a5cc58591f0
Patricia Duran Castro <pcastro@delta09.aulas.gsyc.urjc.es>
2019-02-04 11:21:40 +0100

    Practica_1_clase2_fs_test1_text_ lectura de ficheros

---
+++ Practica-1/fs_test1.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+console.log("MENSAJE NUMERO 1")
+
+//-- Funcion llamada cuando se ha terminado de leer el fichero
+function show_file(err, data) {
+    console.log("----> Comienzo del fichero")
+    console.log(data)
+    console.log("----> FINAL del fichero")
+}
+//-- Leer el fichero. Al terminar se invoca a la función show_file
+fs.readFile('text.txt', 'utf8', show_file);
+
+console.log("----> MENSAJE FINAL")
