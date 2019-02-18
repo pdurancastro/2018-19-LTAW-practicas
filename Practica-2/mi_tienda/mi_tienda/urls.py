e3fcda6a71e0038f9b09bb44574291c0192321fa
Patricia Duran Castro <pcastro@beta06.aulas.gsyc.urjc.es>
2019-02-18 11:53:11 +0100

    Practica 2 bases

---
+++ Practica-2/mi_tienda/mi_tienda/urls.py
@@ -0,0 +1,32 @@
+"""mi_tienda URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+"""Puedo importar mi tienda directamente """
+from django.conf.urls import include, url
+from django.contrib import admin
+from mi_tienda.views import mi_funcion
+from mi_tienda.views import mi_producto
+from mi_tienda.views import saludo
+
+
+
+"""Definir las URLS de la tienda """
+""" r'^admin/ r--> Expresion regular, admin/ --> debe estar al comienzo  """
+urlpatterns = [
+    url(r'^producto/(\d{1,2})/', mi_producto),
+    url(r'^hola/', mi_funcion),
+    url(r'^saludo/', saludo),
+    url(r'^test/', include(admin.site.urls)),
+]
---
+++ Practica-2/mi_tienda/mi_tienda/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+
+
+def mi_funcion(request):
+    html = "Hola! Esto es una prueba"
+
+    return HttpResponse(html)
+
+
+def mi_producto(request, param):
+    numero = int(param)
+    html = "Acceso a producto: %i" % numero;
+    return HttpResponse(html)
+
+"""De momento a pelo luego iria en el html"""
+PLANTILLA = """
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <p>Bienvenido a mi tienda,{{user}} </p>
+
+  </body>
+</html>
+
+"""
+def saludo(request):
+    t = Template(PLANTILLA)
+    c = Context({'user':'Epic Saxo guy'})
+
+    """Plantilla t mas variables en c"""
+    html = t.render(c)
+    return HttpResponse(html)
