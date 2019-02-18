e79cb306fda6a88bc34043c51a77067a30559bcf
Patricia Duran Castro <pcastro@beta06.aulas.gsyc.urjc.es>
2019-02-18 12:56:53 +0100

    Falta leer_html, como el manage esta fuera de la carpeta c->archivo

--- Practica-2/tienda_web/tienda_web/urls.py
+++ Practica-2/tienda_web/tienda_web/urls.py
@@ -1,4 +1,4 @@
-"""tienda_web URL Configuration
+"""mi_tienda URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -12,9 +12,23 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+"""Puedo importar mi tienda directamente """
 from django.conf.urls import include, url
 from django.contrib import admin
+from tienda_web.views import mi_funcion
+from tienda_web.views import mi_producto
+from tienda_web.views import saludo
+from tienda_web.views import leer_html
 
+
+
+"""Definir las URLS de la tienda """
+""" r'^admin/ r--> Expresion regular, admin/ --> debe estar al comienzo  """
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^producto/(\d{1,2})/', mi_producto),
+    url(r'^hola/', mi_funcion),
+    url(r'^saludo/', saludo),
+    url(r'^leer_html/', leer_html),
+    url(r'^test/', include(admin.site.urls)),
 ]
