ff28a8c46ad1186462475f9148358154adaa7298
Patricia Duran Castro <pcastro@beta07.aulas.gsyc.urjc.es>
2019-03-25 11:24:12 +0100

    Gestionando recursos

--- Practica-4/test2/test.js
+++ Practica-4/test2/test.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.send('Probando express... ¡¡¡qué fácil!!!')
 })
 
+app.get('/woala', (req,res) =>{
+  res.send('Funciona!!!!');
+  console.log("Punto de entrada /woala!")
+})
+
 //-- Lanzar servidor
 http.listen(PORT, function(){
   console.log('Servidor lanzado en puerto ' + PORT);
