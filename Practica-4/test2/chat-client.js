dfb08370e9628b190bb07978402e8fc2e779d097
Patricia <patryharuno@gmail.com>
2019-03-25 18:24:54 +0100

    Chat terminado queda ponerlo bonito

--- Practica-4/test2/chat-client.js
+++ Practica-4/test2/chat-client.js
@@ -28,7 +28,7 @@ function main()
   //-- Cuando se reciba un mensaje del servidor se muestra
   //-- en el párrafo
   socket.on('new_message', msg => {
-    display.innerHTML = msg;
+    display.innerHTML = display.innerHTML + msg + "<br>";
   });
 
 }
--- Practica-4/test2/test.js
+++ Practica-4/test2/test.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var usuario = 0;
 
 //--Servir la pagina principal
 app.get('/', function(req, res){
@@ -24,25 +25,45 @@ http.listen(3000, function(){
 io.on('connection', function(socket){
   console.log('--> Usuario conectado!');
 
+  //Dar la Bienvenida al nuevo usuario
+  socket.emit('new_message','Bienvenido!!!! (づ｡◕‿‿◕｡)づ');
+
+  usuario = usuario + 1;
+
   //-- Detectar si el usuario se ha desconectado
   socket.on('disconnect', function(){
     console.log('--> Usuario Desconectado');
+    usuario = usuario - 1 ;
   });
 
   //-- Detectar si se ha recibido un mensaje del cliente
    socket.on('new_message', msg => {
      if (msg == "/help"){
-       console.log("Entrando en help")
-       socket.emit('new_message','Auxilio me desmayo');
+       console.log("Devolviendo lista de comandos")
+       var lista_comandos = "<br>" + "/help" + "<br>" + "/list" + "<br>" + "/hello" + "<br>" +"/date"
+       socket.emit('new_message',lista_comandos);
      }
      else if (msg == "/list") {
-        console.log("Entrando en list")
+       console.log("Entrando en list")
+       socket.emit('new_message',"<br>" +"Numero de usuarios conectados: " + usuario);
+     }
+     else if (msg == "/hello") {
+       console.log("Entrando en hola")
+        socket.emit('new_message',"<br>" +"Holaaaaa " + "~\(≧▽≦)/~");
+
+     }else if (msg == "/date"){
+       console.log("Devolviendo el Recurso Date")
+       var f = new Date();
+       var fecha = "<br>" + f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
+       socket.emit('new_message',fecha);
+
+
      }else{
        //-- Notificarlo en la consola del servidor
        console.log("Mensaje recibido: " + msg)
 
        //-- Emitir un mensaje a todos los clientes conectados
-       io.emit('new_message', msg);
+       io.emit('new_message',"<br>" + msg);
 
      }
 
