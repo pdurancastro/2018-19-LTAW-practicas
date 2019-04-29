fc06a89c914cb44904caeba8b9c1e87f4ed7006b
Patricia Duran Castro <pcastro@beta06.aulas.gsyc.urjc.es>
2019-04-29 12:56:09 +0200

    Models actualizado

--- Practica-2/mi_tienda_web_entrega/mi_tienda/models.py
+++ Practica-2/mi_tienda_web_entrega/mi_tienda/models.py
@@ -9,3 +9,10 @@ class Product (models.Model):
     character = models.CharField(max_length=200, default = "")
     stock = models.IntegerField()
     price = models.FloatField()
+
+#Modelo persona
+class Pedido (models.Model):
+    name_user = models.CharField(max_length=200)
+    Tlf = models.IntegerField()
+    cost = models.IntegerField()
+    order = models.IntegerField()
